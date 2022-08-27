bd80ed30a96125c69465d20e4d84c98b494de06c
Aman <aman20010801@gmail.com>
2022-08-27 20:14:06 +0530

    deployed the smart contract using js

--- smart contract/scripts/deploy.js
+++ smart contract/scripts/deploy.js
@@ -1,22 +1,47 @@
-const hre = require('hardhat')
+// const hre = require('hardhat')
 
-async function main(){
-  const ProfileImageMinterFactory = await hre.ethers.getContractFactory(
-    'ProfileImageNFTs',    
-  )
-  await ProfileImageContract.deployed()
+// async function main(){
+//   const ProfileImageMinterFactory = await hre.ethers.getContractFactory(
+//     'ProfileImageNfts',    
+//   )
+//   // const profileImageContract = await profileImageFactory.deploy()
+//   await ProfileImageContract.deployed()
 
-  console.log(
-   'Profile Image Miner contract deployed to',
-    profileImageContract.address,
+//   console.log(
+//    'Profile Image Miner contract deployed to',
+//     profileImageContract.address,
+//   )
+// }
+
+// ;(async()=>{
+//   try{
+//     await main()
+//     process.exit(0)
+
+//   }catch(error){
+//      console.error(error)
+//     process.exit(1)
+//   }
+// })()
+
+//Code
+const main = async () => {
+  const profileImageFactory = await hre.ethers.getContractFactory(
+    'ProfileImageNfts',
   )
+  const profileImageContract = await profileImageFactory.deploy()
+
+  await profileImageContract.deployed()
+
+  console.log('Profile Image Minter deployed to:', profileImageContract.address)
 }
 
-;(async()=>{
-  try{
+;(async () => {
+  try {
     await main()
-  }catch(error){
-     console.error(error)
+    process.exit(0)
+  } catch (error) {
+    console.error(error)
     process.exit(1)
   }
 })()
